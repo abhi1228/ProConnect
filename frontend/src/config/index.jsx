d5b53cb192b92cdd6985a87784713f903f8e812d
ABHISHEK <ag199680.ag@gmail.com>
2025-07-18 00:21:18 +0530

    updated next.config.js

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,7 +1,7 @@
 const {default:axios}=require("axios");
 
 export const BASE_URL=process.env.NEXT_PUBLIC_API_URL;
-
+console.log(process.env.NEXT_PUBLIC_API_URL);
 export const clientServer=axios.create({
     baseURL:BASE_URL
 })
